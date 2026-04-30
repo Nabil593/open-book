01e53aa3e6ee8f05fa761a1e457c6e3696a5253f
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-04-30 12:58:28 +0600

    Project init

---
+++ src/app/(auth)/layout.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+import Navbar from "@/components/shared/Navbar";
+
+const AuthLayout = ({ children }) => {
+    return (
+        <div>
+            <Navbar />
+            {children}
+        </div>
+    );
+};
+
+export default AuthLayout;
---
+++ src/app/(main)/page.jsx
@@ -0,0 +1,11 @@
+
+
+const MainPage = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    );
+};
+
+export default MainPage;
