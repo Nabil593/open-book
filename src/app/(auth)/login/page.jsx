755139c3860ede6c6a9fcd38c3ade84d42e3b986
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-04-30 13:32:29 +0600

    Login & sign up designed

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const LoginPage = () => {
     return (
-        <div>
-            <h2>Login Page</h2>
+        <div className='flex items-center justify-center flex-col space-y-4 mt-40'>
+            <h2 className='text-3xl font-bold'>Log In</h2>
+            <fieldset className="fieldset mx-auto bg-base-200 border-base-300 rounded-box w-xs border p-4">
+
+                <label className="label">Email</label>
+                <input type="email" className="input" placeholder="Email" />
+
+                <label className="label">Password</label>
+                <input type="password" className="input" placeholder="Password" />
+
+                <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
         </div>
     );
 };
--- src/app/(auth)/registration/page.jsx
+++ src/app/(auth)/registration/page.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 const RegistrationPage = () => {
     return (
-        <div>
-            <h2>Registration Page</h2>
+        <div className='flex items-center justify-center flex-col space-y-4 mt-30'>
+            <h2 className='text-3xl font-bold'>Sign Up</h2>
+            <fieldset className="fieldset mx-auto bg-base-200 border-base-300 rounded-box w-xs border p-4">
+
+                <label className="label">Username</label>
+                <input type="text" className="input" placeholder="Username" />
+
+                <label className="label">Email</label>
+                <input type="email" className="input" placeholder="Email" />
+
+                <label className="label">Photo Url</label>
+                <input type="text" className="input" placeholder="Photo Url" />
+
+                <label className="label">Password</label>
+                <input type="password" className="input" placeholder="Password" />
+
+                <button className="btn btn-neutral mt-4">Sign Up</button>
+            </fieldset>
         </div>
     );
 };
