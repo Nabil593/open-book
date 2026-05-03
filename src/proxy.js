a589bdf980ad5779a000f6b742066e5dcaeeac3b
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-05-03 12:46:20 +0600

    proxy testing

--- src/proxy.js
+++ src/proxy.js
@@ -1,11 +1,18 @@
-import { NextResponse } from 'next/server'
- 
+import { NextResponse } from "next/server";
+import { authClient } from "./lib/auth-client";
+
 // This function can be marked `async` if using `await` inside
 export function proxy(request) {
-    // return NextResponse.next();
-  return NextResponse.redirect(new URL('/login', request.url))
+  const { data: session } = authClient.useSession();
+  const user = session?.user;
+
+  if (user) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 }
- 
+
 export const config = {
-  matcher: ['/books/:path*', '/my-profile'],
-}
+  matcher: ["/books/:path*", "/my-profile"],
+};
